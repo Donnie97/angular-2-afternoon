e1f9aeeda8091adab675210e56169163a5d6c73c
Dante Dufala <thelonelycomet@gmail.com>
2018-02-07 16:10:12 -0700

    DONE

---
+++ app/booked/bookedCtrl.js
@@ -0,0 +1,18 @@
+angular.module('devmtnTravel').controller('bookedCtrl', function($scope, $stateParams, mainSrvc) {
+
+    mainSrvc.getPackageInfo().then( function(response) {
+        console.log(response)
+        $scope.allPackages = response.data;
+
+        if($stateParams.id) {
+            $scope.packageIndex = $scope.allPackages.findIndex( function(package) {
+                return package.id === parseInt( $stateParams.id ); 
+            })
+
+            if ($scope.packageIndex !== -1) {
+                $scope.package = $scope.allPackages[ $scope.packageIndex ];
+            }
+        }
+    })
+
+});
